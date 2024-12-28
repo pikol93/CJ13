52ba0788e4774774d54d06531fb3df35ae6ddb98
pikol93 <piotrdampc93@gmail.com>
2024-12-28 17:41:07 +0100

    Make rotation return to target position when right mouse button is not held

--- src/Player.cs
+++ src/Player.cs
@@ -11,13 +11,32 @@ public partial class Player : Node3D
 
     public static Player Instance { get; private set; }
 
-    public Vector3 ExpectedLookTarget { get; set; }
+    public Vector3 ExpectedLookTarget { get; set; } = new Vector3(0.0f, 1.0f, 0.0f);
     private Camera3D Camera { get; set; }
 
     public override void _Ready()
     {
         Instance = this;
         Camera = GetNode<Camera3D>("Camera");
+    }
+
+    public override void _Process(double delta)
+    {
+        if (Input.IsActionPressed("manual_look"))
+        {
+            return;
+        }
+
+		var normalized = GlobalPosition.MoveToward(ExpectedLookTarget, 1.0f);
+
+        var targetRotationHorizontal = Mathf.Atan2(normalized.X, normalized.Z);
+        var targetRotationVertical = -Mathf.Atan2(normalized.Z, normalized.Y);
+
+        var newRotationHorizontal = Mathf.LerpAngle(Rotation.Y, targetRotationHorizontal, 0.1f);
+        var newRotationVertical = Mathf.LerpAngle(Camera.Rotation.X, targetRotationVertical, 0.1f);
+
+        Rotation = new Vector3(0.0f, newRotationHorizontal, 0.0f);
+        Camera.Rotation = new Vector3(newRotationVertical, 0.0f, 0.0f);
     }
 
     public override void _Input(InputEvent ev)
@@ -33,9 +52,9 @@ public partial class Player : Node3D
         }
 
         var diff = -(mouseMotion.Relative * mouseSensitivity);
-		
-		var newRotationHorizontal = Rotation.Y + diff.X;
-		var newRotationVertical = Mathf.Clamp(Camera.Rotation.X + diff.Y, minVerticalAngle, maxVerticalAngle);
+
+        var newRotationHorizontal = Rotation.Y + diff.X;
+        var newRotationVertical = Mathf.Clamp(Camera.Rotation.X + diff.Y, minVerticalAngle, maxVerticalAngle);
 
         Rotation = new Vector3(0.0f, newRotationHorizontal, 0.0f);
         Camera.Rotation = new Vector3(newRotationVertical, 0.0f, 0.0f);
